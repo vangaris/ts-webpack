0f25af813e1ae7cc0a25800926b179b6f9fed15b
vangaris <vnghenry@gmail.com>
2021-12-19 20:07:23 +0200

    add webpack and fix versoning issues

--- src/index.tsx
+++ src/index.tsx
@@ -1,4 +1,4 @@
-import ReactDom from 'react-dom'
-import {App} from './App'
+import ReactDOM from 'react-dom'
+import { App } from './App'
 
-ReactDom.render(<App/>, document.getElementById('root'))
+ReactDOM.render(<App />, document.getElementById('root'))
