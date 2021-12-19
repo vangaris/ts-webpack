28ca3432f9d1ae0e0816e265b60411f8a9cabdae
vangaris <vnghenry@gmail.com>
2021-12-19 23:00:58 +0200

    config webpack for styling and images etc

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,7 @@
+import "./styles.css"
+import manu from "./manu.jpeg"
+import circle from "./circle.svg"
+
 export function App(){
-    return <h2> App</h2>
+    return<div><h1> Manu</h1> <img src={manu}/> <img src={circle}/> </div> 
 }
